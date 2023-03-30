5bddbb4388c147b6f88237c6fa3fb296d1fa0842
Masumraihan <mdmasumraihan1@gamil.com>
2023-03-31 00:51:27 +0600

    loaded blogs data in json file

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,14 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const Blogs = () => {
     const [blogs,setBlogs] = useState([]);
     useEffect(()=>{
-        
+        const loadBLogs = async() => {
+            const res = await axios.get('data.json');
+            setBlogs(res.data);
+        }
+        loadBLogs();
     },[])
     return (
         <div>
